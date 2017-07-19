de346ffa85b89861accbaed4522dabb1acf32ffe
Hai Vo <haivo@outlook.com>
2017-07-18 22:19:08 -0700

    first draft of assignment

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ud_log_analysis
+Author Hai Vo
+
+# About
+This is a log analysis tool that is used to understand more about the `news`
+db. It queries the `articles`, `authors`, and `log` tables
+
+# Sample
+Can be found in sample_output.txt in the same directory
+
+# How To Run
+1. clone or download the zipped repo
+2. use python2.7
+3. Run with `python2.7 logAnalysis.py`
--- logAnalysis.py
+++ logAnalysis.py
@@ -1,12 +1,21 @@
+#!/usr/bin/env python2.7
+#
+# This is a log analysis that querys the news db
+# queries from the articles, author, and log tables
+# @author Hai Vo
 import psycopg2
 
-class LogAnalysis(Object):
+
+class LogAnalysis(object):
     DBNAME = "news"
 
-    def most_popular_articles(self, count):
-        db = psycopg2.connect(database=DBNAME)
+    # Gets the top count of articles default to 3
+    # sample output [(Candidate is jerk, alleges rival, 338647)]
+    def most_popular_articles(self, count=3):
+        db = psycopg2.connect(database=self.DBNAME)
         c = db.cursor()
-        sql =  """SELECT articles.title, COUNT(log.path)
+        sql = """
+        SELECT articles.title, COUNT(log.path)
         FROM log
         JOIN articles ON log.path = CONCAT('/article/', articles.slug)
         GROUP BY articles.title
@@ -17,10 +26,13 @@ class LogAnalysis(Object):
         db.close()
         return articles
 
+    # Get the author and the amount of views they have
+    # sample output [('Ursula La Multa', Decimal('507594'))]
     def author_view(self):
-        db = psycopg2.connect(database=DBNAME)
+        db = psycopg2.connect(database=self.DBNAME)
         c = db.cursor()
-        sql =  """SELECT authors.name, SUM(articleCount.count)
+        sql = """
+        SELECT authors.name, SUM(articleCount.count) as views
         FROM authors ,
             (SELECT articles.author, COUNT(log.path)
             FROM log
@@ -28,23 +40,34 @@ class LogAnalysis(Object):
             GROUP BY articles.author) AS articleCount
         WHERE authors.id = articleCount.author
         GROUP BY authors.name
-        DESC;"""
+        ORDER BY views
+        DESC ;"""
         c.execute(sql)
         authors = c.fetchall()
         db.close()
         return authors
 
+    # Return the dates that have more than 1 percent errors
+    # sample output [('Jul 17, 2016', Decimal('2.32'))]
     def daily_error(self):
-        db = psycopg2.connect(database=DBNAME)
+        db = psycopg2.connect(database=self.DBNAME)
         c = db.cursor()
-        sql =  """SELECT to_char(badCount.day, 'Mon DD, YYYY'),
-        round(CAST(badCount.count::float / goodCount.count * 100 AS numeric), 2)
-        AS "percent" FROM
-        (SELECT date_trunc('day', time) AS "day", COUNT(*) FROM log
-        WHERE status NOT LIKE '2%' GROUP BY day) AS badCount,
-        (SELECT date_trunc('day', time) AS "day", COUNT(*) FROM log
-        WHERE status LIKE '2%' GROUP BY "day") AS goodCount WHERE
-        badCount.day = goodCount.day
+        sql = """
+        SELECT to_char(badCount.day, 'Mon DD, YYYY'),
+            round(CAST(badCount.count::float /
+            goodCount.count * 100 AS numeric), 2)
+            AS "percent"
+        FROM (SELECT date_trunc('day', time) AS "day", COUNT(*)
+              FROM log
+              WHERE status NOT LIKE '2%'
+              GROUP BY day)
+              AS badCount,
+             (SELECT date_trunc('day', time) AS "day", COUNT(*)
+              FROM log
+              WHERE status LIKE '2%'
+              GROUP BY "day")
+              AS goodCount
+        WHERE badCount.day = goodCount.day
         AND (badCount.count::float / goodCount.count * 100) > 1
         GROUP BY badCount.day, badCount.count, goodCount.count;"""
         c.execute(sql)
@@ -52,23 +75,32 @@ class LogAnalysis(Object):
         db.close()
         return errors
 
+    # Transform return queries to easy to read output
+    def output_logs(self):
+        top_articles = self.most_popular_articles(3)
+        authors_views = self.author_view()
+        log_errors = self.daily_error()
+        print "---------------------------------------------------"
+        print "TOP 3 Articles"
+        print "---------------------------------------------------"
+        for title, view in top_articles:
+            print "%s -- %d views" % (title, view)
+        print "\n"
 
-test = LogAnalysis()
-print test.most_popular_articles(3)
+        print "---------------------------------------------------"
+        print "All Authors' Views"
+        print "---------------------------------------------------"
+        for author, view in authors_views:
+            print "%s -- %d views" % (author, view)
+        print "\n"
 
-    """select articles.title, count(log.path)
-    from log join articles on log.path = concat('/article/', articles.slug)
-    group by articles.title order by count desc limit 3;"""
+        print "---------------------------------------------------"
+        print "Days with Error > 1%"
+        print "---------------------------------------------------"
+        for date, percent in log_errors:
+            print date + " -- " + str(percent) + "% errors"
+        print "\n"
 
-    """
-    select authors.name, sum(articleCount.count) from authors , (select articles.author, count(log.path)
-    from log join articles on log.path = concat('/article/', articles.slug)
-    group by articles.author) as articleCount where authors.id = articleCount.author group by authors.name desc;
-    """
-
-    """
-    select to_char(badCount.day, 'Mon DD, YYYY'), round(CAST(badCount.count::float / goodCount.count * 100 as numeric), 2) as "percent" from
-    (select date_trunc('day', time) as "day", count(*) from log where status not like '2%' group by day) as badCount,
-    (select date_trunc('day', time) as "day", count(*) from log where status like '2%' group by "day") as goodCount where
-    badCount.day = goodCount.day and (badCount.count::float / goodCount.count * 100) > 1 group by badCount.day, badCount.count, goodCount.count;
-    """
+if __name__ == "__main__":
+    test = LogAnalysis()
+    test.output_logs()
